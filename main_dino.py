51fcce3764947b9aaff74244b4b16c0f29f06805
zahrasa <36826126+zahrasa@users.noreply.github.com>
2023-03-19 07:04:21 -0700

    Update main_dino.py
    change aug

--- main_dino.py
+++ main_dino.py
@@ -418,38 +418,32 @@ class DINOLoss(nn.Module):
 
 class DataAugmentationDINO(object):
     def __init__(self, global_crops_scale, local_crops_scale, local_crops_number):
-        flip_and_color_jitter = transforms.Compose([
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomApply(
-                [transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)],
-                p=0.8
-            ),
-            transforms.RandomGrayscale(p=0.2),
-        ])
         normalize = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+            transforms.Normalize((0.485), (0.229)),
         ])
 
         # first global crop
         self.global_transfo1 = transforms.Compose([
-            transforms.RandomResizedCrop(240, scale=global_crops_scale, interpolation=Image.BICUBIC),
+            transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC),
             utils.GaussianBlur(1.0),
-            transforms.ToTensor()
+            normalize,
         ])
+        
         # second global crop
         self.global_transfo2 = transforms.Compose([
-            transforms.RandomResizedCrop(240, scale=global_crops_scale, interpolation=Image.BICUBIC),
+            transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC),
             utils.GaussianBlur(0.1),
             utils.Solarization(0.2),
-            transforms.ToTensor()
+            normalize,
         ])
+        
         # transformation for the local small crops
         self.local_crops_number = local_crops_number
         self.local_transfo = transforms.Compose([
             transforms.RandomResizedCrop(96, scale=local_crops_scale, interpolation=Image.BICUBIC),
             utils.GaussianBlur(p=0.5),
-            transforms.ToTensor()
+            normalize,
         ])
 
     def __call__(self, image):
